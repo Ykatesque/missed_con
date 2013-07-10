c23f6fb8797688f84bde6969dacd7070ad2af07d
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-07-09 18:20:00 -0700

    made rails environment. added gems. deleted rails index file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MissedCon::Application.config.secret_token = '05b97590f2558f8f9da860a104967be7a12758f020782fde20093e1f900084d964d63f3aaee64b43e64ab74592de48d2ad3a03eaefb3e5887e2806b770f97df4'
